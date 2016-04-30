3c5903667d208ad8d5ee8b02e3dbecee5270c25a
frams <frams@163.com>
2016-04-30 22:48:07 +0800

    del fig

--- pl.py
+++ pl.py
@@ -2,14 +2,10 @@ from pylab import *
 import sys
 
 file1 = open("tempactsumstat")
-x = []
 y = []
 
 for line in file1.readlines():
   t1, t2 = line.strip().split(" ")
-  x.append(int(t1))
   y.append(int(t2))
-#plot(y,x,"+")
 plot(y,x)
 savefig(sys.argv[-1])
-#show()
